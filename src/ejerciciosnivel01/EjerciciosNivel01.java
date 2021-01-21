9f17145c59a9a6a8a8aba8a94da2b64ac7e647e6
Javia <Javia@Javi303>
2021-01-21 13:20:36 +0100

    Ejercicio 01 de Programacion Nivel 1 realizado

--- src/ejerciciosnivel01/EjerciciosNivel01.java
+++ src/ejerciciosnivel01/EjerciciosNivel01.java
@@ -10,12 +10,28 @@ package ejerciciosnivel01;
  * @author Javia
  */
 public class EjerciciosNivel01 {
-
+    
+    //declaro un array de integers para hacer pruebas
+    
+    int[] listaNumeros = {3, 31, 200, 20, 5, 99} ;
+    
+    public int maximo(int[] lista){
+        int auxiliar = lista[0]; //El maximo es el primer elemento del array
+        for (int i=0; i< lista.length; i++){
+        if (auxiliar < lista [i]){
+            auxiliar = lista[i];
+        }
+      }
+      return auxiliar;  
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+         EjerciciosNivel01 ejercicio = new EjerciciosNivel01();
+         
+         System.out.println("El maximo es: " + ejercicio.maximo(ejercicio.listaNumeros));
     }
-    
+   
 }
